5225132381a57e14914d9fb1cea5ec1cc130863f
Synthasmagoria <synthas@protonmail.com>
2025-08-29 16:51:48 +0200

    Putting renamable resource paths into lists

--- src/main.zig
+++ src/main.zig
@@ -12,6 +12,10 @@ pub fn printerr(stderr: *std.io.Writer, comptime fmt: []const u8, args: anytype)
     try stderr.print("error: " ++ fmt ++ "\n", args);
 }
 
+pub fn printinfo(stdout: *std.io.Writer, comptime fmt: []const u8, args: anytype) !void {
+    try stdout.print("info: " ++ fmt ++ "\n", args);
+}
+
 pub fn prints(str: []const u8) void {
     print("{s}\n", .{str});
 }
@@ -20,17 +24,26 @@ pub fn printd(a: anytype) void {
     print("{d}\n", .{a});
 }
 
-pub fn init_fsmap(alloc: Allocator, dir_path: []const u8) !std.StringHashMap([]const u8) {
-    var fs_map = std.StringHashMap([]const u8).init(alloc);
-    const dir = try std.fs.openDirAbsolute(dir_path, .{.iterate = true});
+pub fn init_fsmap(alloc: Allocator, dir_path: []const u8) !std.StringHashMapUnmanaged([]const u8) {
+    var fs_map = std.StringHashMapUnmanaged([]const u8).empty;
+    var dir = try std.fs.openDirAbsolute(dir_path, .{.iterate = true});
+    defer dir.close();
     var walker = try dir.walk(alloc);
     while (try walker.next()) |item| {
-        const key = try alloc.dupe(u8, item.path);
-        string.lower(key);
-        try fs_map.put(key, item.path);
+        if (item.kind == .file and
+            std.mem.count(u8, item.path, &.{std.fs.path.sep}) == 2 and
+            std.mem.eql(u8, std.fs.path.extension(item.path), ".yy")) {
+            const k = try alloc.dupe(u8, item.path);
+            string.lower(k);
+            const v = try alloc.dupe(u8, item.path);
+            try fs_map.put(alloc, k, v);
+        }
     }
     return fs_map;
 }
+
+const RELATIVE_PATH_LENGTH_MAX: usize = 2048;
+const BROKEN_PATH_LIST_STARTING_CAPACITY = 1000;
 
 pub fn main() !void {
     var arena_alloc = std.heap.ArenaAllocator.init(std.heap.page_allocator);
@@ -93,30 +106,47 @@ pub fn main() !void {
 
     const resources = root.query_type("resources", zpl.AdtNodeType.Array);
     if (resources == null) {
-        try printerr(stderr, "Couldn't find resources array in YYP", .{});
+        try printerr(stderr, "Couldn't find resources array in project file", .{});
         return;
     }
 
     const absolute_project_directory = std.fs.path.dirname(absolute_project_path) orelse unreachable; // <- is this really true tho?
-    const fs_map = try init_fsmap(alloc, absolute_project_directory);
-    _ = fs_map;
-
     const resources_array_header = resources.?.get_array_header();
-    var broken_paths = try std.ArrayList([]const u8).initCapacity(alloc, cast.i2u(resources_array_header.count));
+    var project_directory = try std.fs.openDirAbsolute(absolute_project_directory, .{});
+    defer project_directory.close();
+    var broken_paths = try std.ArrayList([]const u8).initCapacity(alloc, BROKEN_PATH_LIST_STARTING_CAPACITY);
+    var broken_paths_renamable = try std.ArrayList([]const u8).initCapacity(alloc, BROKEN_PATH_LIST_STARTING_CAPACITY);
+    const fsmap = try init_fsmap(alloc, absolute_project_directory);
+
     for (0..cast.i2u(resources_array_header.count)) |i| {
-
         const node = resources.?.get_array_child(i);
-
         if (node.query("id/path")) |path_node| {
             std.debug.assert(path_node.is_type(zpl.AdtNodeType.String));
             const rel_path = @constCast(std.mem.span(path_node.data.string));
-            const absolute_resource_path = try std.fs.path.join(alloc, &.{absolute_project_directory, rel_path});
+            project_directory.access(rel_path, .{}) catch {
+                if (rel_path.len > RELATIVE_PATH_LENGTH_MAX) {
+                    try printerr(
+                        stderr,
+                        "Relative path length '{d}' exceeded maximum path length '{d}'",
+                        .{rel_path.len, RELATIVE_PATH_LENGTH_MAX});
+                    return;
+                }
+                try broken_paths.append(alloc, try alloc.dupe(u8, rel_path));
 
-            std.fs.accessAbsolute(absolute_resource_path, .{}) catch {
-                try broken_paths.append(alloc, absolute_resource_path);
+                var path_buffer = [_]u8{0} ** RELATIVE_PATH_LENGTH_MAX;
+                const path_buffer_slice = path_buffer[0..rel_path.len];
+                @memcpy(path_buffer_slice, rel_path);
+                string.lower(path_buffer_slice);
+                if (fsmap.contains(path_buffer_slice)) {
+                    try broken_paths_renamable.append(alloc, try alloc.dupe(u8, path_buffer_slice));
+                }
             };
         } else {
-            // FAIL CASE:
+            unreachable; // Every resource entry should have this
         }
     }
+
+    print("Broken paths: {d}, renamable: {d}\n", .{broken_paths.items.len, broken_paths_renamable.items.len});
+    // TODO: Options to view the path lists and unrenamable paths
+
 }
--- src/string.zig
+++ src/string.zig
@@ -1,7 +1,3 @@
-const std = @import("std");
-const Allocator = std.mem.Allocator;
-const assert = std.debug.assert;
-
 pub fn lower(str: []u8) void {
     var i: usize = 0;
     while (i < str.len) : (i += 1) {
@@ -9,34 +5,4 @@ pub fn lower(str: []u8) void {
             str[i] += 'a' - 'A';
         }
     }
-}
-
-pub fn lfind(str: []const u8, chr: u8, from: usize) ?usize {
-    assert(from < str.len);
-    var i = from;
-    while (i > str.len) {
-        i += 1;
-        if (str[i] == chr) {
-            return i;
-        }
-    }
-    return null;
-}
-
-pub fn rfind(str: []const u8, chr: u8, from: usize) ?usize {
-    assert(from < str.len);
-    var i = from + 1;
-    while (i > 0) {
-        i -= 1;
-        if (str[i] == chr) {
-            return i;
-        }
-    }
-    return null;
-}
-
-pub fn duplicate(s: []const u8, alloc: Allocator) ![]const u8 {
-    const s2 = try alloc.alloc(u8, s.len);
-    @memcpy(s2, s);
-    return s2;
 }
